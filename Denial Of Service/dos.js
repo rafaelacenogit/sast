3ebf031fc75c4399aaefa441bb5973a1f5d7c965
rafaelacenogit <60944945+rafaelacenogit@users.noreply.github.com>
2023-01-02 13:45:38 -0300

    Update dos.js

--- Denial Of Service/dos.js
+++ Denial Of Service/dos.js
@@ -6,7 +6,7 @@ router.post("/list-users", (req, res) => {
     var obj = req.body.users;
     var someArr = [];
 
-    // Potential DoS if obj.length is large.
+    // Potential DoS if obj.length is large...
     for (var i = 0; i < obj.length; i++) { 
         someArr.push(obj[i]);
     } 
